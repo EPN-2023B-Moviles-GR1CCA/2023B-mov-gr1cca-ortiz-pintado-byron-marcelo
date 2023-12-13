a05aa5cd2f2ac156bf7fc4c90fc277206339a140
Byron0127 <108595829+Byron0127@users.noreply.github.com>
2023-12-13 16:57:20 -0500

    TareaAndroid

---
+++ Proyecto2023bgr1acc/src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(){
+    println("Hola mundo")
+    // INMUTABLES (NO se reasignan "="
+
+    val immutable: String = "Byron";
+    // immutable : "Ortiz";
+
+
+    // Mutables (Re asignar)
+    var mutable: String = "Ortiz"; I
+    mutable : "Byron" ;
+
+    //Variable primitiva
+
+    val nombreProfesor: String = "Byron Ortiz"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+
+    // Clases Java
+    val fechaNacimiento: Date = Date()
+}
